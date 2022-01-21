1db7e876693884540357eba6ccba1003a9e00c0e
zeeque16 <zeeshanqureshiq@gmail.com>
2022-01-21 18:37:10 +0500

    Todo app using simple list

--- FlaskToDo/app.py
+++ FlaskToDo/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+taskList = ['Hello']
+doneTasks = []
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if (request.method == 'POST'):
+        taskList.append(request.form['todo'])
+
+        return redirect('/')
+    
+    else:
+        return render_template('index.html', taskList=taskList, doneTasks=doneTasks)
+
+@app.route("/delete/<int:index>", methods=['GET', 'POST'])
+def delete(index):
+    if (request.form.get('delete') == 'Delete'):
+        del taskList[index]
+    elif (request.form.get('done') == 'Done'):
+        doneTasks.append(taskList[index])
+        del taskList[index]
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
